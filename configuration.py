983570913c2aeb537dda67db25580f56bb2c12bf
jfpascoal <jfpascoal@gmail.com>
2025-03-03 22:02:39 +0000

    add new classes for Configuration (configuration.py)

---
+++ configuration.py
@@ -0,0 +1,44 @@
+import configparser
+import os
+
+
+class ConfigProperties:
+    CONNECTOR = 'connector'
+    MAPPER = 'connector_mapper'
+    CSV_PATH = 'connector_csv_path'
+    TABLES = 'connector_tables'
+    DB_SYSTEM = 'db_system'
+    DB_SERVER = 'db_server'
+    DB_NAME = 'db_name'
+    DB_SCHEMA = 'db_schema'
+    NAMESPACE = 'avro_namespace'
+    OUTPUT_DIR = 'avro_output_path'
+    ALL_NULLABLE = 'avro_all_nullable'
+
+
+class Configuration:
+    _CONFIG_PATH = os.path.join(os.path.curdir, ".config")
+
+    def __init__(self, props: dict):
+        if not props:
+            props = self.parse_config(self._CONFIG_PATH)
+
+        self.connector = props.get(ConfigProperties.CONNECTOR)
+        self.connector_mapper = props.get(ConfigProperties.MAPPER)
+        self.connector_csv_path = props.get(ConfigProperties.CSV_PATH) or None
+        self.connector_tables = props.get(ConfigProperties.TABLES) or None
+        self.db_system = props.get(ConfigProperties.DB_SYSTEM) or None
+        self.db_server = props.get(ConfigProperties.DB_SERVER) or None
+        self.db_name = props.get(ConfigProperties.DB_NAME) or None
+        self.db_schema = props.get(ConfigProperties.DB_SYSTEM) or None
+        self.avro_namespace = props.get(ConfigProperties.NAMESPACE) or None
+        self.avro_output_path = props.get(ConfigProperties.OUTPUT_DIR) or os.getcwd()
+        self.avro_all_nullable = props.get(ConfigProperties.ALL_NULLABLE) or False
+
+    @staticmethod
+    def parse_config(path: str) -> dict:
+        parser = configparser.ConfigParser(allow_no_value=True)
+        with open(path, 'r') as fh:
+            parser.read_file(fh)
+        config = parser['DEFAULT']
+        return {k: config[k] or None for k in config}
